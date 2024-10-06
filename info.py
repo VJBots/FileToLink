d6b3f6d6d70401b0bfb4a843f1e21c9966b2eba4
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-06 14:40:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '22182189'))
 API_HASH = environ.get('API_HASH', '5e7c4088f8e23d0ab61e29ae11960bf5')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6627318053:AAHQRZcv6Z8A2le5YgwKWEYRqlZcVHb7CeA")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7100825426:AAEFmkfU6-yNk2sBI1V8xXfRpQ3b_pPSqwk")
 
 # Bot settings
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
