3c0b83ecfb208a0c2452f47de5f43c2485472c18
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-06 13:44:06 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,12 @@
 import random
+import humanize
 from Script import script
 from pyrogram import Client, filters, enums
-from info import PICS, LOG_CHANNEL
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
+from info import URL, LOG_CHANNEL, PICS, SHORTNER
+from urllib.parse import quote_plus
+from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
+from TechVJ.util.human_readable import humanbytes
 from database.users_chats_db import db
 from utils import temp
 
@@ -17,3 +22,46 @@ async def start(client, message):
         parse_mode=enums.ParseMode.HTML
     )
     return
+
+
+@Client.on_message(filters.private & filters.incoming)
+async def stream_start(client, message):
+    msg = message 
+    if msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
+        file = getattr(msg, msg.media.value)
+        filename = file.file_name
+        filesize = humanize.naturalsize(file.file_size) 
+        fileid = file.file_id
+        user_id = message.from_user.id
+        username =  message.from_user.mention 
+
+        log_msg = await client.send_cached_media(
+            chat_id=LOG_CHANNEL,
+            file_id=fileid,
+        )
+        fileName = {quote_plus(get_name(log_msg))}
+        if SHORTNER == False:
+            stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+            download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+        else:
+            stream = await get_shortlink(f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}")
+            download = await get_shortlink(f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}")
+        
+        await log_msg.reply_text(
+            text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
+                                                InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
+        )
+        rm=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
+                    InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 📥', url=download)
+                ]
+            ] 
+        )
+        msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥ᴡᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
+
+        await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(msg)), download, stream), quote=True, disable_web_page_preview=True, reply_markup=rm)
