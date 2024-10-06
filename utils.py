cbd35ce713eb67c05c28947b25d7ce8d4ec5e0f7
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-06 13:45:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
+from shortzy import Shortzy
+from info import SHORTLINK_URL, SHORTLINK_API
+
 class temp(object):
     ME = None
     BOT = None
     U_NAME = None
     B_NAME = None
+
+
+async def get_shortlink(link):
+    shortzy = Shortzy(api_key=SHORTLINK_API, base_site=SHORTLINK_URL)
+    link = await shortzy.convert(link)
+    return link
