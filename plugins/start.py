ef135e484313742b4076849804b93fe7a7b4aec1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-06 18:46:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,7 +3,7 @@ import humanize
 from Script import script
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-from info import URL, LOG_CHANNEL, PICS, SHORTLINK
+from info import URL, LOG_CHANNEL, SHORTLINK
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 from TechVJ.util.human_readable import humanbytes
