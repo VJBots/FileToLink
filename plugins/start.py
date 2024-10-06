ca1c8d1fd38e3a70d8209959028f70977dbec0cf
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-06 12:16:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,19 @@
+import random
+from Script import script
+from pyrogram import Client, filters, enums
+from info import PICS, LOG_CHANNEL
+from database.users_chats_db import db
+from utils import temp
 
+@Client.on_message(filters.command("start") & filters.incoming)
+async def start(client, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    await message.reply_photo(
+        photo=random.choice(PICS),
+        caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        reply_markup=reply_markup,
+        parse_mode=enums.ParseMode.HTML
+    )
+    return
